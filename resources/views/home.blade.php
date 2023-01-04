c1cfa19a13deab405671b8ff6f54024c222b881a
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-01-04 12:20:58 +0700

    menambah view home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portofolio | Gabriel Yonathan</title>
+</head>
+<body>
+    <h1>Silahkan login</h1>
+    <a href="auth/redirect">login</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Laravel\Socialite\Facades\Socialite;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 
